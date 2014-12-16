7426f9a54cbda4f82bfae6e1a183fa5e19ed0885
Ian Miller <irmiller22@gmail.com>
2014-12-16 12:52:25 -0500

    changed visited_set to visited_nodes in spec test

--- spec/maze_solver_spec.rb
+++ spec/maze_solver_spec.rb
@@ -15,17 +15,17 @@ describe 'MazeSolver' do
     11x11
   }
 
-  let(:maze_solver) { MazeSolver.new(small_maze) }  
-  
+  let(:maze_solver) { MazeSolver.new(small_maze) }
+
   context "A new maze solver" do
-    it "instantiates with the maze as an attribute" do   
+    it "instantiates with the maze as an attribute" do
       expect(maze_solver.maze).to eq(small_maze)
     end
 
-    it 'instantiates with empty arrays for traveled_path, visited_set, node_queue' do
+    it 'instantiates with empty arrays for traveled_path, visited_nodes, node_queue' do
       expect(maze_solver.traveled_path).to eq([])
       expect(maze_solver.visited_nodes).to eq([])
-      expect(maze_solver.node_queue).to eq([])  
+      expect(maze_solver.node_queue).to eq([])
     end
   end
 
@@ -66,7 +66,7 @@ describe 'MazeSolver' do
     it 'finds the maze end coordinates' do
       expect(maze_solver.end_coordinates).to eq([10,7])
     end
-      
+
   end
 
   context 'Neighbors and nodes' do
@@ -84,20 +84,20 @@ describe 'MazeSolver' do
 
     it 'finds non-blocked neighboring nodes for a given coordinate' do
       expect(maze_solver.neighbors([0,3])).to eq([[1,3]])
-    end 
+    end
   end
 
   context 'Queues and Stacks' do
     describe '#add_to_queues' do
       it 'adds a node to the queues and stacks of the maze solution' do
         maze_solver.add_to_queues([0,3], [0,3])
-        
+
         expect(maze_solver.node_queue).to include([0,3])
         expect(maze_solver.traveled_path[0]).to include([0,3])
         expect(maze_solver.visited_nodes).to include([0,3])
 
         maze_solver.add_to_queues([1,3], [0,3])
-        
+
         expect(maze_solver.node_queue).to include([1,3])
         expect(maze_solver.traveled_path[1]).to include([1,3])
         expect(maze_solver.visited_nodes).to include([1,3])
@@ -117,7 +117,7 @@ describe 'MazeSolver' do
       maze_solver.add_to_queues([0,3])
       maze_solver.move
 
-      expect(maze_solver.node_queue).to include([1,3])        
+      expect(maze_solver.node_queue).to include([1,3])
     end
 
     it "doesn't traverse visited_nodes" do
@@ -146,8 +146,8 @@ describe 'MazeSolver' do
     describe '#solution_path' do
       it 'returns the solution path array' do
         maze_solver.solve
-        
-        # We're using nodes that absolutely must be traveled to in order to confirm a solution path, 
+
+        # We're using nodes that absolutely must be traveled to in order to confirm a solution path,
         # as mazes might include more than one solution.
         solution_must_contain = [[0, 3], [7, 5], [7, 6], [7, 7], [8, 7], [9, 7], [10,7]]
 
@@ -157,7 +157,7 @@ describe 'MazeSolver' do
       end
     end
 
-    # It is possible 
+    # It is possible
     describe '#display_solution_path' do
       it 'prints out the solved maze' do
         # There is another possible solution so don't worry
@@ -179,9 +179,9 @@ describe 'MazeSolver' do
         maze_solver.solve
         # expect($stdout).to receive(:puts).with(@solution_string)
 
-        maze_solver.display_solution_path   
+        maze_solver.display_solution_path
       end
     end
 
   end
-end 
+end
